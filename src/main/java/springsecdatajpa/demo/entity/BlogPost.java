3df33849fc0c6306fd58c1b5480637293e01bf18
TomIsLearningJava <tom.ganemo1987@gmail.com>
2023-01-17 10:58:21 +0100

    added fields to Entity Blogpost regading date and User. altered the Database also to match params. Next step is to clean up and fully decouple

--- src/main/java/springsecdatajpa/demo/entity/BlogPost.java
+++ src/main/java/springsecdatajpa/demo/entity/BlogPost.java
@@ -4,8 +4,10 @@ package springsecdatajpa.demo.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Data
 @AllArgsConstructor
@@ -17,8 +19,18 @@ public class BlogPost {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     @Column(unique = true)
     private String topic;
     private String text;
+
+    @Column(updatable = false)
+    @CreationTimestamp
+    private Date createdAt;
+
+
+    @JoinColumn
+    @ManyToOne(cascade = CascadeType.MERGE)
+    private AppUser appUser;
 
 }
